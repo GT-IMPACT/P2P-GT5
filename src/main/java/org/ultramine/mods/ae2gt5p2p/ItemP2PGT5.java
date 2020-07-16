09c83e145cf650c5573e6253dcb29dcd0da0c4d6
Lex <domian844@gmail.com>
2020-07-16 22:09:17 +0700

    RC#2 - fix AE crash

--- src/main/java/org/ultramine/mods/ae2gt5p2p/ItemP2PGT5.java
+++ src/main/java/org/ultramine/mods/ae2gt5p2p/ItemP2PGT5.java
@@ -1,7 +1,6 @@
 package org.ultramine.mods.ae2gt5p2p;
 
 import appeng.api.AEApi;
-import appeng.api.implementations.items.IItemGroup;
 import appeng.api.parts.IPart;
 import appeng.api.parts.IPartItem;
 import appeng.core.CreativeTab;
@@ -14,46 +13,36 @@ import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
 import java.util.List;
-import java.util.Set;
 
-public class ItemP2PGT5 extends Item implements IPartItem
-{
-	public ItemP2PGT5()
-	{
-		setNoRepair();
-		setUnlocalizedName("ae2gt5p2p_part");
-		setTextureName("appliedenergistics2:ItemPart.P2PTunnel");
-		setCreativeTab(CreativeTab.instance);
-	}
+public class ItemP2PGT5 extends Item implements IPartItem {
+    public ItemP2PGT5() {
+        this.setNoRepair();
+        this.setUnlocalizedName("ae2gt5p2p_part");
+        this.setTextureName("appliedenergistics2:ItemPart.P2PTunnel");
+        this.setCreativeTab(CreativeTab.instance);
+    }
 
-	@Override
-	@SideOnly( Side.CLIENT )
-	public int getSpriteNumber()
-	{
-		return 0;
-	}
+    @SideOnly(Side.CLIENT)
+    public int getSpriteNumber() {
+        return 0;
+    }
 
-	@SideOnly(Side.CLIENT)
-	@SuppressWarnings("unchecked")
-	public void addInformation(ItemStack is, EntityPlayer player, List list, boolean extended)
-	{
-		list.add("Transfers GregTech5 power");
-		list.add("Max voltage - unlimited");
-		list.add("Max amperage - 64");
-		list.add("Without losses and channel requirement");
-		list.add("You can rename it on anvil");
-	}
+    @SideOnly(Side.CLIENT)
+    @SuppressWarnings("unchecked")
+    public void addInformation(ItemStack is, EntityPlayer player, List list, boolean extended) {
+        list.add("Transfers GregTech5 power");
+        list.add("Max voltage - unlimited");
+        list.add("Max amperage - 64");
+        list.add("Without losses and channel requirement");
+        list.add("You can rename it on anvil");
+    }
 
-	@Nullable
-	@Override
-	public IPart createPartFromItemStack(ItemStack is)
-	{
-		return new PartP2PGT5Power(is);
-	}
+    @Nullable
+    public IPart createPartFromItemStack(ItemStack is) {
+        return new PartP2PGT5Power(is);
+    }
 
-	@Override
-	public boolean onItemUse(ItemStack is, EntityPlayer player, World w, int x, int y, int z, int side, float hitX, float hitY, float hitZ )
-	{
-		return AEApi.instance().partHelper().placeBus(is, x, y, z, side, player, w);
-	}
+    public boolean onItemUse(ItemStack is, EntityPlayer player, World w, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+        return AEApi.instance().partHelper().placeBus(is, x, y, z, side, player, w);
+    }
 }
--- src/main/java/org/ultramine/mods/ae2gt5p2p/PartDedicatedP2PTunnel.java
+++ src/main/java/org/ultramine/mods/ae2gt5p2p/PartDedicatedP2PTunnel.java
@@ -10,52 +10,33 @@ import net.minecraft.util.Vec3;
 
 import java.util.ArrayList;
 
-public abstract class PartDedicatedP2PTunnel<T extends PartDedicatedP2PTunnel> extends PartP2PTunnel<T>
-{
-	public PartDedicatedP2PTunnel(ItemStack is)
-	{
-		super(is);
-	}
+public abstract class PartDedicatedP2PTunnel<T extends PartDedicatedP2PTunnel> extends PartP2PTunnel<T> {
+    public PartDedicatedP2PTunnel(ItemStack is) {
+        super(is);
+    }
 
-	@Override
-	public boolean onPartActivate(final EntityPlayer player, final Vec3 pos )
-	{
-		final ItemStack is = player.inventory.getCurrentItem();
-		if(is != null && is.getItem() instanceof IMemoryCard)
-			return super.onPartActivate(player, pos);
-		return false;
-	}
+    public boolean onPartActivate(EntityPlayer player, Vec3 pos) {
+        ItemStack is = player.inventory.getCurrentItem();
+        return (is != null && is.getItem() instanceof IMemoryCard) && super.onPartActivate(player, pos);
+    }
 
-	@SuppressWarnings("unchecked")
-	protected T getInput()
-	{
-		if( this.getFrequency() == 0 )
-		{
-			return null;
-		}
+    protected T getInput() {
+        if (this.getFrequency() == 0L) {
+            return null;
+        } else {
+            try {
+                PartP2PTunnel tunnel = this.getProxy().getP2P().getInput(this.getFrequency());
+                if (this.getClass().isInstance(tunnel)) {
+                    return (T) tunnel;
+                }
+            } catch (GridAccessException var2) {
+            }
 
-		try
-		{
-			final PartP2PTunnel tunnel = this.getProxy().getP2P().getInput( this.getFrequency() );
-			if( this.getClass().isInstance( tunnel ) )
-			{
-				return (T) tunnel;
-			}
-		}
-		catch( final GridAccessException e )
-		{
-			// :P
-		}
-		return null;
-	}
+            return null;
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	protected TunnelCollection<T> getOutputs() throws GridAccessException
-	{
-		if( this.getProxy().isActive() )
-		{
-			return (TunnelCollection<T>) (TunnelCollection) getProxy().getP2P().getOutputs( this.getFrequency(), this.getClass() );
-		}
-		return new TunnelCollection( new ArrayList(), this.getClass() );
-	}
+    protected TunnelCollection<PartP2PTunnel> getOutputs() throws GridAccessException {
+        return this.getProxy().isActive() ? this.getProxy().getP2P().getOutputs(this.getFrequency(), this.getClass()) : new TunnelCollection(new ArrayList(), this.getClass());
+    }
 }
